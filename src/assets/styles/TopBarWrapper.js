51e5591c467bde1806fb81b7c0999c2f766ffa44
John Afolabi <johnafolabi.da@gmail.com>
2021-02-10 14:24:22 +0100

    Build out TopBar component

---
+++ src/assets/styles/TopBarWrapper.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const TopBarWrapper = styled.nav`
+	form > span {
+		position: absolute;
+		margin-left: 15px;
+		height: 25px;
+		display: flex;
+		align-items: center;
+	}
+	input {
+		padding-left: 40px;
+	}
+`;
+
+export default TopBarWrapper;
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Search from "../assets/images/svg/search.svg";
+import { ReactComponent as Notification } from "../assets/images/svg/notification.svg";
+import ProfileImage from "../assets/images/png/profile.png";
+import TopBarWrapper from "../assets/styles/TopBarWrapper";
+
+const TopBar = () => {
+	return (
+		<TopBarWrapper className="navbar navbar-expand-lg navbar-light py-3 px-3">
+			<form className="form-inline col-6 col-xl-4 mr-auto">
+				<span>
+					<img src={Search} alt="search" />
+				</span>
+				<input
+					className="form-control mr-sm-2 w-100"
+					type="search"
+					aria-label="Search"
+					placeholder="Search"
+				/>
+			</form>
+
+			<div>
+				<span className="mr-2 mr-lg-4">
+					<Notification />
+				</span>
+				<img src={ProfileImage} alt="profile" />
+			</div>
+		</TopBarWrapper>
+	);
+};
+
+export default TopBar;
