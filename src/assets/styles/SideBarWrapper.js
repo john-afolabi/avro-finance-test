dd4d5ed19089e3d5dfceaedabd3eccd1b81e815f
John Afolabi <johnafolabi.da@gmail.com>
2021-02-10 13:18:29 +0100

    Build out Sidebar component

---
+++ src/assets/styles/SideBarWrapper.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const SideBarWrapper = styled.div`
+	background-color: #f4f5fa;
+	height: 100vh;
+	padding: unset;
+	.nav-link {
+		color: #637381;
+		font-weight: 700;
+		span:nth-of-type(2) {
+			vertical-align: middle;
+			margin-left: 1rem;
+		}
+	}
+
+	.nav-item:first-of-type {
+		margin-top: 5rem;
+	}
+
+	.nav-item:last-of-type {
+		margin-bottom: 2rem;
+	}
+
+	.nav-item.active {
+		background-color: #eaeffd;
+		border-right: 0.2rem solid #2979f2;
+		.nav-link {
+			color: #2979f2;
+		}
+	}
+`;
+
+export default SideBarWrapper;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { ReactComponent as Dashboard } from "../assets/images/svg/dashboard.svg";
+import { ReactComponent as Companies } from "../assets/images/svg/companies.svg";
+import { ReactComponent as Projects } from "../assets/images/svg/projects.svg";
+import { ReactComponent as Messages } from "../assets/images/svg/messages.svg";
+import { ReactComponent as Settings } from "../assets/images/svg/settings.svg";
+import SideBarWrapper from "../assets/styles/SideBarWrapper";
+
+const Sidebar = () => {
+	return (
+		<SideBarWrapper className="col-2" id="sticky-sidebar">
+			<div className="sticky-top h-100">
+				<ul class="nav flex-column h-100">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">
+							<span>
+								<Dashboard />
+							</span>
+							<span>Dashboard</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							<span>
+								<Companies />
+							</span>
+							<span>Companies</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							<span>
+								<Projects />
+							</span>
+							<span>Projects</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							<span>
+								<Messages />
+							</span>
+							<span>Messages</span>
+						</a>
+					</li>
+
+					<li className="nav-item mt-auto">
+						<a className="nav-link" href="#">
+							<span>
+								<Settings />
+							</span>
+							<span>Settings</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</SideBarWrapper>
+	);
+};
+
+export default Sidebar;
